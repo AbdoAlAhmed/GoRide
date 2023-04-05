6bf662ba5b2a680e51fe721563df5f68da4ed4c8
AbdoAlAhmed <abdoahmed.bzn@gmail.com>
2023-04-05 13:32:17 +0200

    upload

--- app/src/main/java/com/theideal/goride/MyApplication.kt
+++ app/src/main/java/com/theideal/goride/MyApplication.kt
@@ -2,6 +2,8 @@ package com.theideal.goride
 
 import android.app.Application
 import com.google.android.libraries.places.api.Places
+import com.google.firebase.FirebaseApp
+import com.google.firebase.messaging.FirebaseMessaging
 import timber.log.Timber
 import java.util.*
 
@@ -10,6 +12,13 @@ class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        FirebaseApp.initializeApp(this)
+        FirebaseMessaging.getInstance().token.addOnSuccessListener { token ->
+           Timber.i( "FCM token: $token")
+        }.addOnFailureListener { e ->
+           Timber.i( "some thing wrong here body: $e")
+        }
 
         if (!Places.isInitialized()) {
             Places.initialize(applicationContext, api_Key, Locale.getDefault())
