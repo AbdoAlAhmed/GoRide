31c9d2678d918f3fb656fb6762b81d22749c6776
AbdoAlAhmed <abdoahmed.bzn@gmail.com>
2023-04-06 01:27:02 +0200

    Driver Activity 1-  Home Driver Fragment

--- app/src/main/java/com/theideal/goride/ui/driver/home/HomeDriverFragment.kt
+++ app/src/main/java/com/theideal/goride/ui/driver/home/HomeDriverFragment.kt
@@ -1,4 +1,4 @@
-package com.theideal.goride.ui.driver
+package com.theideal.goride.ui.driver.home
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
---
+++ app/src/main/java/com/theideal/goride/ui/driver/home/HomeDriverViewModel.kt
@@ -0,0 +1,4 @@
+package com.theideal.goride.ui.driver.home
+
+class HomeDriverViewModel {
+}
