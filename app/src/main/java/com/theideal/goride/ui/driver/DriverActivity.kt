7e6b322bfcce8e83a0afdf7aa329c1ac03a5a5d1
AbdoAlAhmed <abdoahmed.bzn@gmail.com>
2023-04-05 22:36:13 +0200

    Driver finish some settings

--- app/src/main/java/com/theideal/goride/ui/driver/DriverActivity.kt
+++ app/src/main/java/com/theideal/goride/ui/driver/DriverActivity.kt
@@ -1,12 +1,23 @@
 package com.theideal.goride.ui.driver
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.theideal.goride.R
+import com.theideal.goride.databinding.ActivityHomeDriverBinding
 
 class DriverActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeDriverBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_driver)
+        binding = ActivityHomeDriverBinding.inflate(layoutInflater)
+        supportActionBar?.hide()
+        setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_driver) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomAppBarDriver.setupWithNavController(navController)
+
     }
 }
--- app/src/main/java/com/theideal/goride/ui/driver/ServicesDriverFragment.kt
+++ app/src/main/java/com/theideal/goride/ui/driver/ServicesDriverFragment.kt
@@ -1,12 +1,13 @@
 package com.theideal.goride.ui.driver
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.theideal.goride.R
-class ProfileDriverFragment : Fragment() {
+
+class ServicesDriverFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +19,8 @@ class ProfileDriverFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_driver, container, false)
+        return inflater.inflate(R.layout.fragment_serivces_driver, container, false)
     }
+
 
 }
