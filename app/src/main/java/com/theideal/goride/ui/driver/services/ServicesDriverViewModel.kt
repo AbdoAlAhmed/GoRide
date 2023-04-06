c9da8ef1d8f35a78cca7a559e091d465e08f00a1
AbdoAlAhmed <abdoahmed.bzn@gmail.com>
2023-04-06 17:12:43 +0200

    viewmodel and viewModel Factory in services Driver Done

---
+++ app/src/main/java/com/theideal/goride/ui/driver/services/ServicesDriverViewModel.kt
@@ -0,0 +1,22 @@
+package com.theideal.goride.ui.driver.services
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.theideal.goride.model.CardViewData
+import com.theideal.goride.model.ServicesDriverFirebase
+
+class ServicesDriverViewModel(private val db:ServicesDriverFirebase) : ViewModel(){
+
+    private val _servicesDriver = MutableLiveData<ArrayList<CardViewData>>()
+    val servicesDriver: MutableLiveData<ArrayList<CardViewData>>
+        get() = _servicesDriver
+
+
+
+
+    fun getServicesDriver(){
+        db.getRideServicesServicesFragment {
+            _servicesDriver.value = it
+        }
+    }
+}
---
+++ app/src/main/java/com/theideal/goride/ui/driver/services/ServicesDriverViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.theideal.goride.ui.driver.services
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.theideal.goride.model.ServicesDriverFirebase
+
+class ServicesDriverViewModelFactory(private val servicesFirebase: ServicesDriverFirebase) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ServicesDriverViewModel::class.java)) {
+            return ServicesDriverViewModel(servicesFirebase) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
